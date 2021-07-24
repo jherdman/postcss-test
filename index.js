469c5ccb228f08b8d922e7dc910ef4c829b1432d
James Herdman <james.herdman@gmail.com>
2021-07-24 12:05:57 -0400

    Reproduction

--- index.js
+++ index.js
@@ -2,4 +2,16 @@
 
 module.exports = {
   name: require('./package').name,
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+    app.options = app.options || {};
+
+    app.options.postcssOptions = {
+      compile: {
+        enabled: true,
+        plugins: [{ module: require('postcss-import') }]
+      }
+    };
+  }
 };
